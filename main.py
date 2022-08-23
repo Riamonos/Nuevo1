c3e9a508a2284736df1646646c4849000c442d3d
ThattaPy <sergioalvarez3000@gmail.com>
2022-08-23 05:47:56 +0200

    inicializado proyecto

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-def set_name():
-    return input("¿Tu Nombre?")
+
+def message(_message):
+    print('--- ' + _message + ' ---')
 
 
-def print_hi():
-    name = set_name()
-    print(f'Hola, {name}!!')
+def init_sale():
+    message('Seleccionar gominolas')
+
 
 
 if __name__ == '__main__':
-    print_hi()
-
+    message('Hola, soy tu máquina de gominola <3')
+    is_admin = bool(input('admin?'))    # Cuando es 0 usuario, 1 es admin
+    if is_admin == '1212':
+        pass
+    elif not is_admin:
+        init_sale()
