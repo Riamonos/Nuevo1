6a57dd854f03ba7a1599047b21ffcf3680ca7e39
ThattaPy <sergioalvarez3000@gmail.com>
2022-08-23 05:19:55 +0200

    creada funcion setname

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def set_name():
+    return input("¿Tu Nombre?")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_hi():
+    name = set_name()
+    print(f'Hola, {name}!!')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
